deaecc2e31d1802f7ada579f4138241cc518d053
Yeongjin Song <jinijani@Yeongjins-MacBook-Air.local>
2022-07-02 10:47:29 +0900

    g

--- examples.py
+++ examples.py
@@ -2,5 +2,8 @@ import sys
 sys.path.extend(['/Users/jinijani/PycharmProjects/git_project/ML/Linear-Classifiers-with-Visualization-on-A-Plane'])
 from Perceptron import twoD_coordinates_Perceptron
 from linear_GNB import twoD_coordinates_lGNB
+from logistic_r import twoD_coordinates_logistic_r
 
-twoD_coordinates_Perceptron()
+a = twoD_coordinates_Perceptron()
+a = twoD_coordinates_lGNB()
+a = twoD_coordinates_logistic_r()
